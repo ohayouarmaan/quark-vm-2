4acad146dbeb5f3cc0eef23ef5dda0828fd3b685
Armaan Gupta <ohayouarmaan@gmail.com>
2025-03-16 01:47:25 +0530

    feat: fixed simple bugs regarding Instructions

--- src/machine/quark_vm.rs
+++ src/machine/quark_vm.rs
@@ -39,7 +39,7 @@ impl QuarkVM {
     pub fn store_file(&mut self, file_name: &str) {
         let mut f = fs::File::create(file_name).expect("QUARMVM: Failed to create the file");
         for instruction in self.instructions.iter() {
-            f.write_all(&instruction.to_bytes()).expect(&format!("QUARMVM: Error while writing instruction {:?}", instruction));
+            f.write_all(&instruction.to_bytes()).unwrap_or_else(|_| panic!("QUARMVM: Error while writing instruction {:?}", instruction));
         }
     }
 
@@ -49,12 +49,12 @@ impl QuarkVM {
         f.read_to_end(&mut buf).expect("QUARMVM: Error while reading the file");
         let mut i = 0;
         println!("BUFFER: {:?}", buf);
+        let mut ins = vec![];
         while i < buf.iter().len() {
             let instruction = buf[i];
             i += 1;
             let argument_length = buf[i];
             i += 1;
-            println!("INSTRUCTION: {:?} ARG_LEN: {:?}", instruction, argument_length);
             let mut args: Vec<u16> = vec![];
             for x in 0..argument_length {
                 let arg = u16::from_be_bytes([buf[i + x as usize], buf[i + x as usize + 1]]);
@@ -70,8 +70,9 @@ impl QuarkVM {
                     None
                 }
             };
-            self.instructions.push(instruction);
+            ins.push(instruction);
         }
+        self.instructions = ins;
     }
 
     pub fn determine_function(&mut self) {
@@ -85,7 +86,7 @@ impl QuarkVM {
             InstructionType::INST_PUSH => {
                 match &self.instructions[self.pc as usize].values {
                     Some(value) => {
-                        self.push_stack(value[0] as u16);
+                        self.push_stack(value[0]);
                         self.pc += 1;
                     }
                     None => {
@@ -119,6 +120,10 @@ impl QuarkVM {
                 self.pc += 1;
             },
         }
+    }
+
+    pub fn debug_stack(&self) {
+        println!("sp: {:?} stack: {:?}", self.sp, &self.stack[0..(self.sp as usize + 1)]);
     }
 
     pub fn run(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let mut quark_machine = machine::machine_types::QuarkVM::new();
     quark_machine.load_file("./test.qasm");
     quark_machine.run();
-    println!("stack: {:?}", &quark_machine.stack[0..(quark_machine.sp as usize)]);
+    quark_machine.debug_stack();
 }
